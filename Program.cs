06b43fa35c86e371415c2e531e0da8a9905953ec
mandara11 <mandaram2345@gmail.com>
2022-08-09 12:34:19 +0530

    Added:UC1

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using InventoryManagement;
+
+namespace InventoryMangement
+{
+    class Program  //UC1
+    {
+        public static void Main(string[] args)
+        {
+            FetchInventoryDetails fetchInventoryDetails = new FetchInventoryDetails();
+
+
+            string filepath = "C:\\Users\\DELL\\source\\repo\\InventoryManagement\\InventoryManagement\\Data.json";
+
+            Rice data = fetchInventoryDetails.Read(filepath);
+
+            Console.WriteLine("Types of Rice :");
+            for (int i = 0; i < data.typesofRice.Count; i++)
+            {
+
+                Console.WriteLine(data.typesofRice[i].name);
+                Console.WriteLine(data.typesofRice[i].price);
+                Console.WriteLine(data.typesofRice[i].weight);
+
+                Console.WriteLine("--------------");
+            }
+
+            Console.WriteLine("Types of Pulses :");
+            for (int i = 0; i < data.typesofPulses.Count; i++)
+            {
+
+                Console.WriteLine(data.typesofPulses[i].name);
+                Console.WriteLine(data.typesofPulses[i].price);
+                Console.WriteLine(data.typesofPulses[i].weight);
+
+                Console.WriteLine("--------------");
+            }
+
+            Console.WriteLine("Types of Wheat :");
+            for (int i = 0; i < data.typesofPulses.Count; i++)
+            {
+
+                Console.WriteLine(data.typesofWheat[i].name);
+                Console.WriteLine(data.typesofWheat[i].price);
+                Console.WriteLine(data.typesofWheat[i].weight);
+
+                Console.WriteLine("--------------");
+            }
+
+        }
+    }
+}
